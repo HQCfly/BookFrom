f1df2dfdd2e379e3d7a7c8a5747a0e5748dce4aa
HQCfly <2249629867@qq.com>
2019-02-19 14:38:15 +0800

    1添加传统的增删改 2添加form形式的增删改

--- app01/views.py
+++ app01/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render,redirect
 from .models import *
 
 # Create your views here.
+'''传统模式的增删改
 def books(request):
     book_list=Book.objects.all()
     return render(request,"book.html",locals())
@@ -50,3 +51,57 @@ def editbook(request,edit_book_id):
     publish_list = Publish.objects.all()
     author_list = Author.objects.all()
     return render(request,"edit.html",locals())
+    
+'''
+from django.forms import ModelForm
+from django.forms import widgets as wid
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = "__all__"
+        labels = {"title":"书籍名称","price":"价格"}
+        widgets = {
+            "title": wid.TextInput(attrs={"class": "form-control"}),
+            "price": wid.TextInput(attrs={"class": "form-control"}),
+            "date": wid.TextInput(attrs={"class": "form-control", "type": "date"}),
+            "publish": wid.Select(attrs={"class": "form-control"}),
+            "authors": wid.SelectMultiple(attrs={"class": "form-control"}),
+        }
+        error_messages = {
+            "title":{"required":"不能为空"}
+        }
+
+def books(request):
+    book_list=Book.objects.all()
+    return render(request,"book.html",locals())
+
+def addbook(request):
+    if request.method=="POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/books/")
+        else:
+            return render(request,"add.html",locals())
+    form = BookForm()
+    return render(request,"add.html",locals())
+
+def editbook(request,edit_book_id):
+    edit_book = Book.objects.filter(pk=edit_book_id).first()
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=edit_book)
+        if form.is_valid():
+            form.save()  # edit_book.update(request.POST)
+            return redirect("/books/")
+
+    form = BookForm(instance=edit_book)
+    return render(request, "edit.html", locals())
+
+
+
+
+
+
+
+
+
